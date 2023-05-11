195092f84c614068afd1778ef79a71f1a7d732cc
Seth Grief-Albert <sethgriefalbert@gmail.com>
2023-05-11 11:43:57 -0400

    Fix iteration over list
    The list was modified when iterating over it

--- number_of_ways.py
+++ number_of_ways.py
@@ -20,8 +20,8 @@ def numberOfWays(startPos: int, endPos: int, k: int) -> int:
 
     # loop k times
     for i in tqdm(range(k)):
-        for path in paths:
-            new_path = path.copy()
+        for _ in range(len(paths)):
+            new_path = paths.pop(0)
             last_position = new_path[-1]
 
             # exits fast if not going to make to end
@@ -32,7 +32,7 @@ def numberOfWays(startPos: int, endPos: int, k: int) -> int:
             new_path_left = new_path + [last_position - 1] # changed 
 
             # path that goes to the right
-            new_path_right = new_path + [last_position - 1]
+            new_path_right = new_path + [last_position + 1]
 
             # add paths to the left and right
             paths.append(new_path_left)
